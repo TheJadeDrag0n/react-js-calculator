7df54b09678c5ee5e7adce7e9a9aa4e4af3f5d5b
James C <47540316+TheJadeDrag0n@users.noreply.github.com>
2022-10-27 11:29:22 +1300

    Update ButtonKeypad.js

--- src/Components/ButtonKeypad.js
+++ src/Components/ButtonKeypad.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function ButtonKeypad(props) {
     let buttons = [];
     for (let i = 0; i <= 9; i++) {
@@ -19,26 +18,4 @@ function ButtonKeypad(props) {
         </div>
     );
 }
-=======
-function ButtonKeypad(props) {
-    let buttons = [];
-    for (let i = 0; i <= 9; i++) {
-        buttons.push(
-            <button 
-                onClick={() => props.updateCalculation(i)} 
-                key={i}
-            >
-                {i}
-            </button>
-        );
-    }
-    return (
-        <div>
-            {buttons}
-            <button onClick={() => props.updateCalculation(".")}>.</button>
-            <button onClick={() => props.updateCalculation("del")}>Del</button>
-        </div>
-    );
-}
->>>>>>> f12148decc10ecf0ae70fb3abf2860a85e5962be
 export default ButtonKeypad;
